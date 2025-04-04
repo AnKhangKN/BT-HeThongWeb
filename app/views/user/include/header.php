db4aad57a37c8391573001c5881d9c153325d071
Phan An Khang <minecraftcopyright1302@gmail.com>
2025-04-04 18:49:05 +0700

    pppp

--- app/views/user/include/header.php
+++ app/views/user/include/header.php
@@ -9,11 +9,14 @@
     <script src="../vendor/jquery-3.7.1/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="../vendor/fontawesome-free-6.7.2-web/css/all.min.css">
 
+    <!-- css -->
+    <link rel="stylesheet" href="../app/views/user/assets/css/style.css">
+
     <title>Trang chủ</title>
 </head>
 
 <body>
-    <header id="header">
+    <header id=" header">
         <div class="topbar" style=" background-color: #ff5c39;">
             <div class="d-flex justify-content-between align-items-center m-auto text-white"
                 style="min-height: 30px; max-width: 1300px; font-size: 13px;">
@@ -30,9 +33,12 @@
             <div class="d-flex justify-content-between align-items-center m-auto"
                 style="height: 80px; max-width: 1300px;">
 
-                <div class="logo" style="width: 80px; height: 46px;">
-                    <img src="../public/assets/images/Logo.webp" alt="" class="w-100 h-100" style="object-fit: cover;">
-                </div>
+                <a href="index.php?page=home">
+                    <div class="logo" style="width: 80px; height: 46px;">
+                        <img src="../public/assets/images/Logo.webp" alt="" class="w-100 h-100"
+                            style="object-fit: cover;">
+                    </div>
+                </a>
 
                 <div class="search-box d-flex justify-content-center align-items-center w-50">
 
@@ -49,21 +55,53 @@
 
                 <div class="tools d-flex align-items-center text-white" style="font-size: 18px;">
 
-                    <div class="cart position-relative">
-                        <div class="total_products_in_cart position-absolute d-flex justify-content-center align-items-center rounded-5"
-                            style="font-weight: 500; top: -6px; right: 12px;width: 20px; height: 20px; background-color:#E0E0E0; font-size: 14px;">
-                            <div class="total_product text-dark">0</div>
+                    <a href="index.php?page=cart" style="color: #fff; text-decoration: none">
+                        <div class="cart position-relative">
+                            <div class="total_products_in_cart position-absolute d-flex justify-content-center align-items-center rounded-5"
+                                style="font-weight: 500; top: -6px; right: 12px;width: 20px; height: 20px; background-color:#E0E0E0; font-size: 14px;">
+                                <div class="total_product text-dark">0</div>
+                            </div>
+                            <div class="me-3 cart-icon p-1 d-flex align-items-center rounded-5 justify-content-center"
+                                style=" background-color: #ff5c39; width: 40px; height: 40px;">
+                                <i class="fa-solid fa-basket-shopping"></i>
+                            </div>
                         </div>
-                        <div class="me-3 cart-icon p-1 d-flex align-items-center rounded-5 justify-content-center"
-                            style=" background-color: #ff5c39; width: 40px; height: 40px;">
-                            <i class="fa-solid fa-basket-shopping"></i>
+                    </a>
+
+                    <div class="position-relative">
+                        <a href="index.php?page=information" style="color: #fff; text-decoration: none">
+                            <div class="user-info d-flex justify-content-center rounded-5 align-items-center"
+                                style="width: 40px; height: 40px; background-color: #ff5c39;">
+                                <i class="fa-regular fa-user"></i>
+                            </div>
+                        </a>
+
+                        <!-- modal information no login -->
+                        <div id="modalInformationDetailNoLogin"
+                            class="modalInformationDetail text-dark position-absolute d-flex">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a style="text-decoration: none" class="text-dark" href="./signup">Đăng kí</a>
+                                </li>
+                                <li>
+                                    <a style="text-decoration: none" class="text-dark" href="./login">Đăng nhập</a>
+                                </li>
+                            </ul>
                         </div>
+
+                        <div id="modalInformationDetailLogin"
+                            class="modalInformationDetail text-dark position-absolute d-none">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a style="text-decoration: none" class="text-dark" href="./logout/logout.php">Đăng
+                                        xuất</a>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
 
-                    <div class="user-info d-flex justify-content-center rounded-5 align-items-center"
-                        style="width: 40px; height: 40px; background-color: #ff5c39;">
-                        <i class="fa-regular fa-user"></i>
-                    </div>
+
                 </div>
 
             </div>
@@ -77,16 +115,17 @@
                     <div class="d-flex justify-content-center m-auto align-items-center w-50 "
                         style="font-weight: 500;">
                         <div class="d-flex w-25 justify-content-center" style="line-height: 44px;">
-                            <a href="index.php?page=home">Trang chủ</a>
+                            <a style="color: #333; text-decoration: none" href="index.php?page=home">Trang chủ</a>
                         </div>
                         <div class="d-flex w-25 justify-content-center" style="line-height: 44px;">
-                            <a href="index.php?page=product">Sản phẩm</a>
+                            <a style="color: #333; text-decoration: none" href="index.php?page=product">Sản phẩm</a>
                         </div>
                         <div class="d-flex w-25 justify-content-center" style="line-height: 44px;">
-                            <a href="index.php?page=warranty">Bảo Hành</a>
+                            <a style="color: #333; text-decoration: none" href="index.php?page=warranty">Bảo Hành</a>
                         </div>
                         <div class="d-flex w-25 justify-content-center" style="line-height: 44px;">
-                            <a href="index.php?page=introduction">Giới thiệu</a>
+                            <a style="color: #333; text-decoration: none" href="index.php?page=introduction">Giới
+                                thiệu</a>
                         </div>
                     </div>
 
